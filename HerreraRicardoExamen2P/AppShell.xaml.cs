47bde0ab3ea163a51ea5405056b20ae68e69081c
laboratorio <laboratorio@UPOAULA10710>
2025-06-05 07:12:12 -0500

    Agregar archivos de proyecto.

---
+++ HerreraRicardoExamen2P/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HerreraRicardoExamen2P
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
