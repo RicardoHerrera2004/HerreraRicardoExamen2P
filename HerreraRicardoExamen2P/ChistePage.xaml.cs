38657ef6bfda6deca50423697f572af309440e06
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-06-05 08:08:09 -0500

    Quinto commit: Creacion de la clase, los metodos y boton que se utilizara para el consumo de las apis de los chistes

--- HerreraRicardoExamen2P/ChistePage.xaml.cs
+++ HerreraRicardoExamen2P/ChistePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace HerreraRicardoExamen2P;
 
 public partial class ChistePage : ContentPage
@@ -5,10 +7,21 @@ public partial class ChistePage : ContentPage
 	public ChistePage()
 	{
 		InitializeComponent();
-	}
+        ObtenerChiste();
+    }
+    private async void ObtenerChiste()
+    {
+      return;
+    }
 
     private void ContarChistes_Clicked(object sender, EventArgs e)
     {
+		ObtenerChiste();
+    }                                                                                                                                                                                                                                                           
 
+    public class chiste
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
     }
 }
