e40818573409841671e8bdc841a5c1f3f7d0d864
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-06-05 08:17:34 -0500

    Septimo commit: Creacion de un try y catch para el manejo de errores dentro del consumo de la api "ObtenerChiste"

--- HerreraRicardoExamen2P/ChistePage.xaml.cs
+++ HerreraRicardoExamen2P/ChistePage.xaml.cs
@@ -11,11 +11,19 @@ public partial class ChistePage : ContentPage
     }
     private async void ObtenerChiste()
     {
+        try
+        { 
         var client = new HttpClient();
         var respuesta = await client.GetFromJsonAsync<Chiste>("https://official-joke-api.appspot.com/random_joke");
 
         if (respuesta != null)
             ChisteL.Text = $" Chiste: {respuesta.setup}\n\n Respuesta: {respuesta.punchline}"; ;
+        }
+        catch 
+        { 
+            await DisplayAlert("Error", "No se pudo obtener el chiste. Inténtalo de nuevo más tarde.", "OK"); 
+            return; 
+        }
     }
 
     private void ContarChistes_Clicked(object sender, EventArgs e)
